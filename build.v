5d98273550a960b7564f5bad54f89d7da2330dc1
Diego F. Goberna <github@feiss.be>
2020-09-01 02:16:39 +0200

    v script finished

--- build.v
+++ build.v
@@ -1,5 +1,3 @@
-
-
 all_dirs := walk_ext('projects', '')
 mut mdirs := map[string]int
 for _, d in all_dirs{
@@ -10,32 +8,55 @@ for _, d in all_dirs{
 }
 
 dirs := mdirs.keys()
-//NAMES := map[string]string
+mut names := map[string]string
 
-//tpl_file := read_file('projects/index.tpl.html')
+tpl := read_file('projects/index.tpl.html') or { "" }
 
 for dir in dirs{
-  mut meta := read_file('projects/$dir/meta.html')
+  mut meta := read_file("projects/$dir/meta.html") or {
+    println("cannot read $dir/meta.html")
+    return
+  }
   project_name := meta.all_before('\n')
-  //meta:= meta.all_after('\n')delete(0)
-  //html := meta.join()
-  println(project_name)
-}
-/*
+  html:= meta.all_after('\n')
+  names[dir] = project_name
 
-  meta = meta_file.readlines()
-  project_name = meta.pop(0).strip()
-  html = ''.join(meta)
-  meta_file.close()
-  NAMES[dir] = project_name
-
-  index_file = open('projects/' + dir + '/index.html', 'w')
-  index_file.write(
-    tpl
-    .replace('$TITLE', project_name)
-    .replace('$HTML', html)
-  )
+  mut index_file := create('projects/$dir/index.html') or {
+    print('cannot create $dir/index.html')
+    return
+  }
+  index_file.write(tpl.replace('\$TITLE', project_name).replace('\$HTML', html))
   index_file.close()
-  print('- ' + project_name)
-print(str(len(dirs))+' projects processed')
-*/
+  println('- $project_name')
+}
+
+println(dirs.len.str() + ' projects processed')
+
+
+// generate projects.html
+
+item_html := '<a title="\$NAME" href="projects/\$FOLDER" style="background-image: url(projects/\$FOLDER/thumb.jpg)"><img src="res/th.png"></a>\n'
+
+tpl2 := read_file('projects.tpl.html') or {
+  println("cannot read projects.tpl.html")
+  return
+}
+mut projects_file := create('projects.html') or {
+    print('cannot create projects.html')
+    return
+  }
+
+projects_file.write(tpl2.all_before('\$PROJECT_LIST'))
+mut num_projects := 0
+lines := tpl2.find_between('\$PROJECT_LIST', '\$END_PROJECT_LIST').split('\n')
+for line in lines {
+  folder := line.trim_space()
+  if folder!='' && folder in names {
+    projects_file.write(item_html.replace('\$FOLDER', folder).replace('\$NAME', names[folder]))
+    num_projects++
+  }
+}
+projects_file.write(tpl2.all_after('\$END_PROJECT_LIST'))
+projects_file.close()
+println('\nprojects.html written with ' + num_projects.str() + ' projects.')
+
